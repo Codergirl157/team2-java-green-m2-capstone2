aa02511245c42d315505668c25a5e0018f6e910d
Aolani Daniels <aolanidaniels@gmail.com>
2021-10-24 12:38:52 -0400

    before pull

--- etc/excelsior_venues.sql
+++ etc/excelsior_venues.sql
@@ -349,3 +349,20 @@ JOIN reservation ON space.id = reservation.space_id
 WHERE venue_id = 1
 AND reservation.start_date > '2021-10-21'
 AND reservation.end_date < '2021-10-31' 
+
+
+   SELECT space.id, space.venue_id, space.name, space.is_accessible, space.open_from, space.open_to, space.daily_rate::money::numeric::float8, space.max_occupancy  FROM space 
+   WHERE space.venue_id = 5 
+    AND space.id NOT IN (SELECT space.id FROM space 
+LEFT JOIN reservation ON space.id = reservation.space_id 
+   WHERE ('2021-10-17' <= reservation.end_date 
+                AND '2021-10-25' >= reservation.start_date) 
+                OR EXTRACT(MONTH FROM CAST('2021-10-17' AS DATE)) < space.open_from 
+                OR EXTRACT(MONTH FROM CAST('2021-10-25' AS DATE)) > space.open_to 
+                OR space.max_occupancy < 50) 
+                ORDER BY space.daily_rate DESC 
+                LIMIT 5;
+
+SELECT name
+FROM space
+WHERE venue_id = 7;
